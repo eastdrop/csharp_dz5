6f23d47d766c6370ff16af6b9ecc6527f586dd0d
eastdrop <lyokha7777@gmail.com>
2023-02-28 18:52:21 +0300

    Added task34

---
+++ task34/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+
+int[] GenArray(int size)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = random.Next(100, 999);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int CountChet(int[] array)
+{
+    int count = 0;
+    for (int i = 0;i < array.Length;i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int[] array = GenArray(10);
+PrintArray(array);
+Console.WriteLine(CountChet(array));
